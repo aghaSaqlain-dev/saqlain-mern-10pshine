4dfc8944c6772a001d5ff9cf9c2e5664f10ed5fb
aghaSaqlain <03i08459082@gmail.com>
2025-07-02 18:30:33 +0500

    notes integration complete

--- back-end/src/routes/notesRoute.ts
+++ back-end/src/routes/notesRoute.ts
@@ -15,13 +15,13 @@ import {
 const router = express.Router();
 
 router.get('/notes',asyncHandler(getAllNotes)); // Get all notes
-router.get('/notes/"id', asyncHandler(getNoteById)); // Get a specific note by ID
-router.post('notes', asyncHandler(createNote)); // Create a new note
-router.post('notes/:id/pin', asyncHandler(pinNote)); // pin/unpin a note
-router.post('notes/:id/restore',asyncHandler(restoreNote)); // retore from trash
-router.patch('notes/:id', asyncHandler(updateNote)); // Update a specific note by ID
-router.delete('notes/:id', asyncHandler(deleteNote)); // Delete a specific note by ID
-router.delete('notes/:id/force', asyncHandler(forceDeleteNote)); // permanently delete a specific note by ID
+router.get('/notes/:id', asyncHandler(getNoteById)); // Get a specific note by ID
+router.post('/notes', asyncHandler(createNote)); // Create a new note
+router.post('/notes/:id/pin', asyncHandler(pinNote)); // pin/unpin a note
+router.post('/notes/:id/restore',asyncHandler(restoreNote)); // retore from trash
+router.patch('/notes/:id', asyncHandler(updateNote)); // Update a specific note by ID
+router.delete('/notes/:id', asyncHandler(deleteNote)); // Delete a specific note by ID
+router.delete('/notes/:id/force', asyncHandler(forceDeleteNote)); // permanently delete a specific note by ID
 
 
 export default router;
